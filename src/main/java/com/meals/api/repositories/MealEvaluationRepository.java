fa99d7823b189b1c1f991f7644392c85aadeab35
Kalebe <kaka.leebe@gmail.com>
2025-04-06 22:24:41 -0300

    Reshapes the way average rating requests are handled

--- src/main/java/com/meals/api/repositories/MealEvaluationRepository.java
+++ src/main/java/com/meals/api/repositories/MealEvaluationRepository.java
@@ -3,6 +3,7 @@ package com.meals.api.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.meals.api.domain.MealEvaluation;
@@ -10,4 +11,7 @@ import com.meals.api.domain.MealEvaluation;
 @Repository
 public interface MealEvaluationRepository extends JpaRepository<MealEvaluation, Integer> {
     List<MealEvaluation> findByMealId(int mealId);
+
+    @Query("SELECT AVG(e.evaluation) FROM MealEvaluation e WHERE e.mealId = :mealId")
+Double mealAverageEvaluation(int mealId);
 }
